4ecf874929a1232aa03e32e2665d290ed6d9d5df
ADMARIN2870 <amarin@megacable.com.ar>
2024-11-05 10:53:23 -0300

    Correciones
    * Envío correcto del form no muestra
       todos los datos, solo mensaje.
    * El cuadro de confirmación "¿Desea
       enviar esta respuesta?" borra el form
       aunque se presione "NO".

--- javaScript/contacto.js
+++ javaScript/contacto.js
@@ -34,6 +34,8 @@ function validar(){
     if( telefono.value.length <=0 || telefono.value.length >11){
         let errorTelefono = document.getElementById('errorTelefono');
         errorTelefono.style.display = 'block';
+
+        errorFlag =true;
     }
     if(mail.value=="" || !validMail.test(mail.value)){
         let errorMail = document.getElementById('errorMail');
@@ -46,25 +48,21 @@ function validar(){
 
     //Respuesta
     if(!errorFlag){
+        confirmar.style.display = 'block';
         let texto = document.createElement('p');
         let reseña = document.createElement('p');
         texto.innerHTML = `${apellido.value}, ${nombre.value} valora la pagina como '${seleccion}' y deja la siguiente reseña: `;
         reseña.innerHTML = `${correccion.value}`;
         confirmar.insertBefore(texto, confirmar.firstChild);
         texto.appendChild(reseña);
-        confirmar.style.display = 'block';
         confirmar.style.boxShadow ='0px 0px 40px 5px  rgba(0, 0, 0, 0.9)';
-        nombre.value = '';
-        apellido.value='';
-        mail.value='';
-        telefono.value='';
-        correccion.value='';
         setTimeout(() => {
             confirmar.removeChild(texto);
             confirmar.removeChild(reseña);
         }, 7000);
         const form = document.getElementById('form');
         form.style.backgroundColor= 'rgba(0, 0, 0, 0.3)';
+        
        
 }
 
@@ -79,21 +77,25 @@ function limpiar(event){
     limpiarSpan.style.display = 'none';
     let result=document.getElementById('result');
     result.style.display = 'none';
-}
+ }
 
 function spawn(){
-    const form = document.getElementById('form');
-    form.style.backgroundColor= 'white';
     confirmar.style.display = 'none';
     let result=document.getElementById('result');
     result.style.display = 'block';
-    let main = document.getElementById('main');
-    main.style.backgroundColor= 'gba(0, 0, 0, 0.3)';
 
-
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('telefono').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('correccion').value = '';
+    document.getElementsByName('opinion').forEach((radio) => (radio.checked = false));
 }
+
+
 function elimniar(){
     confirmar.style.display = 'none';
+
     const form = document.getElementById('form');
     form.style.backgroundColor= 'white';
     
